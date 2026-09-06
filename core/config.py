063c775fc161cadec6e45da36c3b35a347b54a37
tcqx <minawagnair@gmail.com>
2026-09-06 19:54:18 +0200

    Modify ASCII logo in config.py
    Updated ASCII logo for the application.

--- core/config.py
+++ core/config.py
@@ -6,11 +6,9 @@ APP_FULL_NAME = "TCQX TERMINAL"
 APP_VERSION = "1.0.0"
 APP_USER = "tcqx"
 
-
 GITHUB_URL = "https://github.com/tcqx"
 GUNSLOL_URL = "https://guns.lol/tcqx"
 DISCORD_INVITE = "https://discord.gg/TON-INVITE"
-
 
 COLOR_PRIMARY = "bright_green"
 COLOR_SECONDARY = "green"
@@ -24,18 +22,28 @@ COLOR_BACKGROUND_TEXT = "grey37"
 
 PROMPT_STYLE = f"bold {COLOR_PRIMARY}"
 
-
 IS_WINDOWS = platform.system().lower() == "windows"
 
 
 ASCII_LOGO = r"""
-████████╗ ██████╗ ██████╗ ██╗  ██╗
-╚══██╔══╝██╔════╝██╔═══██╗╚██╗██╔╝
-   ██║   ██║     ██║   ██║ ╚███╔╝
-   ██║   ██║     ██║   ██║ ██╔██╗
-   ██║   ╚██████╗╚██████╔╝██╔╝ ██╗
-   ╚═╝    ╚═════╝ ╚═════╝ ╚═╝  ╚═╝
+████████╗ ██████╗██████╗ ██╗  ██╗
+╚══██╔══╝██╔════╝██╔══██╗╚██╗██╔╝
+   ██║   ██║     ██████╔╝ ╚███╔╝
+   ██║   ██║     ██╔═══╝  ██╔██╗
+   ██║   ╚██████╗██║     ██╔╝ ██╗
+   ╚═╝    ╚═════╝╚═╝     ╚═╝  ╚═╝
 
-              T C Q X
-            T E R M I N A L
+████████╗ ██████╗ ██████╗
+╚══██╔══╝██╔════╝██╔══██╗
+   ██║   ██║     ██████╔╝
+   ██║   ██║     ██╔═══╝
+   ██║   ╚██████╗██║
+   ╚═╝    ╚═════╝╚═╝
+
+██╗  ██╗
+╚██╗██╔╝
+ ╚███╔╝
+ ██╔██╗
+██╔╝ ██╗
+╚═╝  ╚═╝
 """
